60a0bbfaba304c75c7d5b17d419573414a56af79
Gia Huy <huystem2006@gmail.com>
2026-04-22 20:05:50 +0700

    drop

--- DATABASE/SIEUTHI.sql
+++ DATABASE/SIEUTHI.sql
@@ -1,10 +1,3 @@
 CREATE DATABASE Supermarket;
-CREATE TABLE nhan_vien (
-    id CHAR(3) PRIMARY KEY,
-    ten VARCHAR(50) NOT NULL
-); /*vai lon luo lm duoc roi */
-CREATE TABLE Khach(
-id_Khach char(3) NOT NULL
-)
-/*Tuấn mặt lồn */
-/*tuan yeu huy*/
+DROP table Khach;
+DROP table Nhan_vien;
