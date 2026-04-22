7917908f867abced3e96e698cce19ae9f44d2dee
Giahuy6406 <email_github_cua_ban>
2026-04-22 18:33:05 +0600

    comment

--- DATABASE/SIEUTHI.sql
+++ DATABASE/SIEUTHI.sql
@@ -2,7 +2,7 @@ CREATE DATABASE Supermarket;
 CREATE TABLE nhan_vien (
     id CHAR(3) PRIMARY KEY,
     ten VARCHAR(50) NOT NULL
-);
+); /*vai lon luo lm duoc roi */
 CREATE TABLE Khach(
 id_Khach char(3) NOT NULL
 )
